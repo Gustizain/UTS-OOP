dd5d81e8a77eec500a5f83b6ddd66fea0ae01129
Gustizain <74238476+Gustizain@users.noreply.github.com>
2020-11-11 12:45:16 +0700

    UTS OOP

---
+++ UTS OOP/src/uts/oop/Persegi.java
@@ -0,0 +1,19 @@
+package uts.oop;
+
+public class Persegi {
+    public static void main(String[] args) {
+        int sisi;
+        int hasilkeliling;
+        int hasilluas;
+        
+        sisi = 10;
+        
+        hasilkeliling = 4 * sisi;
+        hasilluas = sisi * sisi;
+        
+        System.out.println ("Hasil Keliling Persegi : " + hasilkeliling);
+        System.out.println ("Hasil Luas Persegi");
+       
+    }
+    
+}
